95f6745324e3cae917ecad58b0659a40c674d6da
Jing Wang <jingw9327@gmail.com>
2023-11-10 17:40:55 -0800

    finished project 2 of section7.

---
+++ 02-code-along/06-Modal/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+console.log(modal);
+
+const openModalHandler = function () {
+  console.log("Button clicked.");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModalHandler = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModalHandler);
+
+btnCloseModal.addEventListener("click", closeModalHandler);
+overlay.addEventListener("click", closeModalHandler);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModalHandler();
+  }
+});
